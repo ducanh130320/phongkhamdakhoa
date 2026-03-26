c664d72debd827a8888f1ea056aaaa1bf7f87bd1
Do Duc Anh <doducanh48@gmail.com>
2026-03-26 16:23:00 +0700

    Updates

--- _next/static/ORB-JAn_I1S2CbB2jnpPN/_buildManifest.js
+++ _next/static/ORB-JAn_I1S2CbB2jnpPN/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/phongkhamdakhoa//_next/:path+",
+        "destination": "/phongkhamdakhoa/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
